9e989b7362e8464f8a56c90f7cfed7493540c9ce
PatrickEifler <patrickeifler@gmail.com>
2016-07-18 09:30:43 +0200

    Update app id

--- src/config/project.js
+++ src/config/project.js
@@ -2,6 +2,6 @@
 
 export default {
 	name: 'Scratch Combos Web Client',
-	id: '9186f1e4-93ef-4a75-a0a8-203f807d473b',
+	id: '9356f1e4-73ef-4675-a0a8-203f807d473b',
 	redirectURI: 'http://localhost:3000/login'
 };
